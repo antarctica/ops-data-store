bd6f77b78903b1684adca83accbb449aeecf7eaa
Felix Fennell - BAS <felnne@bas.ac.uk>
2023-10-01 19:58:34 +0000

    Adding minimal Python CLI
    Responds to `—help` and `—version` commands only initially.

---
+++ src/ops_data_store/config.py
@@ -0,0 +1,16 @@
+from importlib.metadata import version
+
+from dotenv import load_dotenv
+
+
+class Config:
+    """Application configuration."""
+
+    def __init__(self) -> None:
+        """Create Config instance and load options from dotenv file."""
+        load_dotenv()
+
+    @property
+    def VERSION(self) -> str:
+        """Application version."""
+        return version("ops-data-store")
