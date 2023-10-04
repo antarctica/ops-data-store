e93f5f2dc4362d526f3536a26faa6ee4d84d0cdc
Felix Fennell <felnne@bas.ac.uk>
2023-10-04 20:17:06 +0100

    Adding logging to DB run command
    For completeness

--- src/ops_data_store/cli/db/__init__.py
+++ src/ops_data_store/cli/db/__init__.py
@@ -35,15 +35,26 @@ def check() -> None:
 @app.command(help="Execute contents of an SQL against DB.")
 def run(input_path: Annotated[Path, typer.Option()]) -> None:
     """Load contents of SQL file and execute against DB."""
+    logger.info("Loading file contents and running inside DB.")
+    logging.info(f"Input path: {input_path.resolve()}")
+
     if not input_path.exists():
+        logging.error("Input path not found.")
         print(f"No. Input path '{input_path.resolve()}' does not exist.")
         raise typer.Abort()
     if not input_path.is_file():
+        logging.error("Input path not a file.")
         print(f"No. Input path '{input_path.resolve()}' is not a file.")
         raise typer.Abort()
 
-    print(f"Executing SQL from input file at '{input_path.resolve()}' against app DB.")
-    with psycopg.connect(config.DB_DSN) as conn, conn.cursor() as cur:
-        cur.execute(input_path.read_text())
+    try:
+        print(f"Executing SQL from input file at '{input_path.resolve()}' against app DB.")
+        with psycopg.connect(config.DB_DSN) as conn, conn.cursor() as cur:
+            cur.execute(input_path.read_text())
+    except (psycopg.ProgrammingError, psycopg.OperationalError) as e:
+        logger.error(e, exc_info=True)
+        print("No. Error running commands in input file.")
+        raise typer.Abort() from e
 
+    logging.info("Input file loaded and executed successfully.")
     print("Complete.")
